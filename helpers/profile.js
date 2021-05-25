4c4f19bad481dd773dfd8ca318cbe063459f982a
LSuazo1 <luissuazo80@yahoo.com>
2021-05-25 16:45:00 -0600

    Portafolio terminado

---
+++ helpers/profile.js
@@ -0,0 +1,85 @@
+export const skills = [
+  {
+    skill: "Javascript",
+    percentage: 100,
+  },
+  {
+    skill: "java",
+    percentage: 80,
+  },
+  {
+    skill: "Nodejs",
+    percentage: 90,
+  },
+  {
+    skill: "css",
+    percentage: 80,
+  },
+  {
+    skill: "React",
+    percentage: 80,
+  },
+  {
+    skill: "Spring Boot",
+    percentage: 80,
+  },
+  {
+    skill: "Jquery",
+    percentage: 70,
+  },
+];
+
+export const experiences = [
+  {
+    title: "Practica de curso de java",
+    description:
+      "La practica fue realizada en un call center, donde realizamos la creacion de una pagina web hecha en spring boot",
+    from: 2019,
+    to: 2020,
+  },
+];
+
+export const projects = [
+  {
+    name: "Pixabay",
+    description:
+      "Proyecto de practica consumiento la api de pixabay",
+    image: "pixabay.png",
+    link:"https://pixabay-react-practica.netlify.app/"
+  },
+  {
+    name: "Citas Veterinaria",
+    description:
+      "Proyecto de practica de una veterinaria para agendar cita, estas se almacenan en el local storage.",
+    image: "citas.png",
+    link:"https://proyecto-citas.netlify.app/"
+  },
+  {
+    name: "Cotizador de criptomonedas",
+    description:
+      "Proyecto de practica cortizador de criptomonedas ",
+    image: "criptomonedas.png",
+    link:"https://criptomonedasreact1.netlify.app/"
+  },
+  {
+    name: "Control Presupuesto Semanal",
+    description:
+      "Proyecto de practica control de presupuesto semanal,en este proyecto podemos llevar el control de los gastos semanales",
+    image: "presupuesto.png",
+    link:"https://spa-presupuesto.netlify.app/"
+  },
+  {
+    name: "Practica Maquetacion",
+    description:
+      "Proyecto donde realizamos la maquetacion de una pagina web de un curso de master en css",
+    image: "practica-maquetacion.png",
+    link:"https://maquetacion-2.netlify.app/"
+  },
+  {
+    name: "Frases Breaking Bad",
+    description:
+      "Proyecto donde consumimos una api de las frases de Breaking Bad y podemos ir obteniendo  una frase siempre que queramos",
+    image: "breaking.png",
+    link:"https://react-frases.netlify.app/"
+  },
+];
---
+++ pages/index.js
@@ -0,0 +1,130 @@
+import Layout from "../components/Layout";
+import Link from "next/link";
+
+import { skills, experiences, projects } from "../helpers/profile";
+
+const Index = () => (
+  <Layout>
+    {/* Header Card */}
+    <header className="row">
+      <div className="col-md-12">
+        <div className="card card-body bg-secondary text-light animate__animated animate__fadeIn">
+          <div className="row">
+            <div className="col-md-2">
+              <img src="/Lsuazo.jpeg" alt="" className="img-fluid" />
+            </div>
+            <div className="col-md-10">
+              <h1>Luis Suazo</h1>
+              <h3>Full Stack Developer</h3>
+              <p>
+              mi portafolio es una representación de todo lo que he aprendido y logrado como estudiante de ingeniería en sistemas.
+              </p>
+              {/* <Link href="/hireme">
+                <a className="btn btn-outline-light">Hire Me</a>
+              </Link> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    {/* Second section */}
+    <section className="row">
+      <div className="col-md-4 py-2">
+        <div className="card bg-light animate__animated animate__fadeInLeft">
+          <div className="card-body">
+            <h1>Skills</h1>
+
+            {/* Skill Progress  */}
+            {skills.map(({ skill, percentage }, i) => (
+              <div className="py-3" key={i}>
+                <h5>{skill}</h5>
+                <div className="progress ">
+                  <div
+                    className="progress-bar"
+                    role="progressbar"
+                    style={{ width: `${percentage}%` }}
+                    aria-valuenow="50"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  ></div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div className="col-md-8 py-2">
+        {/* Experience */}
+        <div className="card bg-light animate__animated animate__fadeInRight">
+          <div className="card-body">
+            <h1>Experience</h1>
+
+            <ul>
+              {/* List Item Experience */}
+              {experiences.map(({ title, from, to ,description}, index) => (
+                <li key={index}>
+                  <h3>{title}</h3>
+                  <h5>
+                    {from} {to ? `- ${to}` : "- current"}
+                  </h5>
+                  <p>
+                    {description}
+                  </p>
+                </li>
+              ))}
+            </ul>
+            {/* <Link href="/hireme">
+              <a className="btn btn-light">Know More</a>
+            </Link> */}
+          </div>
+        </div>
+      </div>
+    </section>
+    {/* Porfolio */}
+    <section>
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card card-body bg-dark">
+            <div className="row">
+              <div className="col-md-12 my-2">
+                <h1 className="text-center text-light">Portfolio</h1>
+              </div>
+              {projects.map(({ name, description, image,link }, index) => (
+                <div className="col-md-4 p-2" key={index}>
+                  <div className="card h-100">
+                    <div className="overflow">
+                      <img
+                        src={`/${image}`}
+                        alt=""
+                        className="card-img-top"
+                      />
+                    </div>
+                    <div className="card-body">
+                      <h3>{name}</h3>
+                      <p>{description}</p>
+                      <Link href={link}>
+                      <a  target="_blank">Know More</a>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              ))}
+
+              {/* <div className="col-md-12 mt-4">
+                <div className="text-center">
+                  <Link href="/portfolio">
+                    <a className="btn btn-outline-light">More Projects</a>
+                  </Link>
+                </div>
+              </div> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </Layout>
+);
+
+export default Index;
